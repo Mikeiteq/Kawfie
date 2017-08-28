c4f31d04a0a2fe214b97e9f9cdc29768a753d5cf
greengardmichael <greengardmichael@gmail.com>
2017-08-27 18:03:35 -0700

    Commit message, hooray!

--- Kawfie/Kawfie/Form1.vb
+++ Kawfie/Kawfie/Form1.vb
@@ -1,5 +1,18 @@
-﻿Public Class Form1
+﻿Imports Discord
+
+Public Class Form1
+
+    Dim WithEvents discord As New DiscordClient
+
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        test
+
+        Try
+            discord.Connect("MzUxMTMyODcwMjU5MTEzOTg0.DIT1-g.qqIEft3YKe1H_vnXw3Np_q4UfBU", TokenType.Bot)
+        Catch ex As Exception
+
+            MessageBox.Show(ex.Message)
+
+        End Try
+
     End Sub
 End Class
